66a20bdbb6d674e03edfce2a4fef22656b2e47fd
Simon Ittmann <simon.ittmann@byteleaf.de>
2020-10-24 18:47:01 +0200

    test framework changed to junit 4

---
+++ src/test/kotlin/de/byteleaf/renamefiles/control/service/CreationDateServiceTest.kt
@@ -0,0 +1,13 @@
+package de.byteleaf.renamefiles.control.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CreationDateServiceTest {
+
+    @Test
+    fun getCreationDate() {
+        assertTrue(true);
+    }
+}
